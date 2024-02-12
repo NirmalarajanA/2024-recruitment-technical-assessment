86f1d6f7b800f82753add77936bc50ec1a6b3e69
NirmalarajanA <129946247+NirmalarajanA@users.noreply.github.com>
2024-02-12 16:42:27 +1100

    Update task.py

--- backend/py/task.py
+++ backend/py/task.py
@@ -13,21 +13,64 @@ class File:
 Task 1
 """
 def leafFiles(files: list[File]) -> list[str]:
-    return []
+    nameList = []
+    for file in files:
+        # Using find() to check if its a file and not a folder as that is differentiated by a '.something'
+        if file.name.find('.') > 0:
+            nameList.append(file.name)
 
+    return nameList
 
 """
 Task 2
 """
 def kLargestCategories(files: list[File], k: int) -> list[str]:
-    return []
+    # Create a Dictionary and store how many times a category appears 
+    dic = {}
+    for file in files:
+        for categories in file.categories:
+            if categories in dic:
+                dic[categories] += 1
+            else:
+                dic[categories] = 1
 
+    # Ensures that the k categories taken are sorted by number of and alphabetically 
+    sortedDic = dict(sorted(dic.items(), key = lambda x: (-x[1], x[0])))
+
+    # Take the first k elements in the sorted dictionary 
+    returnList = []
+    for key in sortedDic:
+        returnList.append(key)
+        if (len(returnList) == k):
+            break
+
+    return returnList
 
 """
 Task 3
 """
 def largestFileSize(files: list[File]) -> int:
-    return 0
+    childSizeDic = {}
+    parentSizeDic = {}
+
+    # Initialising Dictionaries 
+    for file in files: 
+        if file.parent == -1:
+            parentSizeDic[file.id] = file.size 
+        else: 
+            childSizeDic[file.id] = file.size 
+    
+    # Find the total for all childs 
+    for file in files: 
+        if file.parent in childSizeDic: 
+            childSizeDic[file.parent] += file.size 
+
+    # Find the total for all parents 
+    for file in files:
+        if file.parent in parentSizeDic:
+            parentSizeDic[file.parent] += childSizeDic[file.id]
+
+    return max(parentSizeDic.values())
 
 
 if __name__ == '__main__':
